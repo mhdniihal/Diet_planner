e3375f61555e6b9c34b82398fd3b594d90796200
Nihal <nihal01mohammed@gmail.com>
2026-05-29 23:02:07 +0530

    Datasets are readed

---
+++ src/data_cleaning.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+health_df = pd.read_csv("C:\\Documents\\Projects\\Diet_planner\\data\\Health & BMI Dataset.csv")
+
+food_df = pd.read_csv("C:\\Documents\\Projects\\Diet_planner\\data\\Food Nutrition Dataset.csv")
+
+calorie_df = pd.read_csv("C:\\Documents\\Projects\\Diet_planner\\data\\Calorie Requirement Dataset.csv")
