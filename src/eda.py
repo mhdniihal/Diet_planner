720115ce1bbb029cc2d6f5267ec42e6bd7f26241
Nihal <nihal01mohammed@gmail.com>
2026-05-30 14:05:55 +0530

    Goal distribution

--- src/eda.py
+++ src/eda.py
@@ -6,7 +6,14 @@ health_df = pd.read_csv("data/cleaned/health_cleaned.csv")
 food_df = pd.read_csv("data/cleaned/food_cleaned.csv")
 calorie_df = pd.read_csv("data/cleaned/calorie_cleaned.csv")
 
+# BMI distribution
 plt.figure(figsize=(8,5))
 sns.histplot(health_df['BMI'], bins=20, kde=True)
 plt.title("BMI Distribution")
+plt.show()
+
+# Goal distribution
+plt.figure(figsize=(8,5))
+sns.countplot(x='Goal', data=health_df)
+plt.title("Goal Distribution")
 plt.show()
