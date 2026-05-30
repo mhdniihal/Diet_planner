ac057c7be0624c082033967e578ef07168562ec4
Nihal <nihal01mohammed@gmail.com>
2026-05-30 19:41:25 +0530

    Bmi category define

--- src/bmi_calculator.py
+++ src/bmi_calculator.py
@@ -3,3 +3,18 @@ def calculate_bmi(weight, height):
     height_m = height / 100
     bmi = weight / (height_m ** 2)
     return round(bmi, 2)
+
+
+def bmi_category(bmi):
+    
+    if bmi < 18.5:
+        return "Underweight"
+
+    elif bmi < 25:
+        return "Normal Weight"
+
+    elif bmi < 30:
+        return "Overweight"
+
+    else:
+        return "Obese"
