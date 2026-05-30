82c921a688865e35d2fd20044f0e6dcde75656b6
Nihal <nihal01mohammed@gmail.com>
2026-05-30 12:42:11 +0530

    Data cleaned

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,7 +1,41 @@
 import pandas as pd
 
-health_df = pd.read_csv("C:\\Documents\\Projects\\Diet_planner\\data\\Health & BMI Dataset.csv")
+health_df = pd.read_csv(r"C:\Documents\Projects\Diet_planner\data\Health & BMI Dataset.csv")
 
-food_df = pd.read_csv("C:\\Documents\\Projects\\Diet_planner\\data\\Food Nutrition Dataset.csv")
+food_df = pd.read_csv(r"C:\Documents\Projects\Diet_planner\data\Food Nutrition Dataset.csv")
 
-calorie_df = pd.read_csv("C:\\Documents\\Projects\\Diet_planner\\data\\Calorie Requirement Dataset.csv")
+calorie_df = pd.read_csv(r"C:\Documents\Projects\Diet_planner\data\Calorie Requirement Dataset.csv")
+
+'''print(health_df.info())
+print(food_df.info())
+print(calorie_df.info())
+
+print(health_df.head())
+print(food_df.head())
+print(calorie_df.head())
+
+health_df = health_df.loc[:, ~health_df.columns.str.contains('^Unnamed')]
+food_df = food_df.loc[:, ~food_df.columns.str.contains('^Unnamed')]
+calorie_df = calorie_df.loc[:, ~calorie_df.columns.str.contains('^Unnamed')]
+
+print(health_df.isnull().sum())
+print(food_df.isnull().sum())
+print(calorie_df.isnull().sum())
+
+health_df.fillna(
+    health_df.select_dtypes(include='number').mean(),
+    inplace=True
+)
+
+health_df.fillna("Unknown", inplace=True)
+
+health_df.drop_duplicates(inplace=True)
+food_df.drop_duplicates(inplace=True)
+calorie_df.drop_duplicates(inplace=True)
+
+health_df["Gender"] = health_df["Gender"].str.capitalize()
+health_df["Goal"] = health_df["Goal"].str.title()'''
+
+print(health_df.shape)
+print(food_df.shape)
+print(calorie_df.shape)
