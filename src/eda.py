43289d4411fb0581f6ea6e72d52d4e7ac0fc16eb
Nihal <nihal01mohammed@gmail.com>
2026-05-30 14:02:03 +0530

    New file created for EDA activities

---
+++ src/eda.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+health_df = pd.read_csv("data/cleaned/health_cleaned.csv")
+food_df = pd.read_csv("data/cleaned/food_cleaned.csv")
+calorie_df = pd.read_csv("data/cleaned/calorie_cleaned.csv")
+
