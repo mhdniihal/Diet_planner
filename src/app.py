a9125136bbbab03104c5965f355cf1874fca0b53
Nihal <nihal01mohammed@gmail.com>
2026-06-01 16:55:59 +0530

    bmi measures set

--- src/app.py
+++ src/app.py
@@ -28,3 +28,40 @@ goal = st.selectbox(
     ["Bulk", "Cut", "Fat Loss"]
 )
 
+# BMI Calculation
+bmi = 0
+def calculate_bmi(weight, height):
+
+    height_m = height / 100
+
+    bmi = weight / (height_m ** 2)
+
+    return round(bmi, 2)
+
+if st.button("Calculate BMI"):
+
+    bmi = calculate_bmi(weight, height)
+
+    st.success(f"Your BMI is {bmi}")
+
+
+# BMI Category
+def bmi_category(bmi):
+
+    if bmi < 18.5:
+        return "Underweight"
+
+    elif bmi < 25:
+        return "Normal Weight"
+
+    elif bmi < 30:
+        return "Overweight"
+
+    else:
+        return "Obese"
+    
+category = bmi_category(bmi)
+
+st.info(f"Category: {category}")
+
+
